56fdc01a17b1170585a0a79c40519dc263c14409
RagingDad <50980481+RagingDad@users.noreply.github.com>
2024-03-24 21:27:58 +0100

    Add files via upload

---
+++ vagrant.dir/scripts/ssh-nodes.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Deploy keys to kubernetes-nodes to eneble connection from rke as root
+mkdir /root/.ssh
+chmod 700 /root/.ssh/
+chown root:root /root/.ssh
+cat /tmp/id_rsa.pub >> /root/.ssh/authorized_keys
+chmod 400 /root/.ssh/authorized_keys
+chown root:root /root/.ssh/authorized_keys
