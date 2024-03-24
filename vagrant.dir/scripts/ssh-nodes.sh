e073082c972aa015391a658eff0fc82fe5de6656
RagingDad <50980481+RagingDad@users.noreply.github.com>
2024-03-24 21:22:56 +0100

    Delete vagrant.dir directory

--- vagrant.dir/scripts/ssh-nodes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Deploy keys to kubernetes-nodes to enable connection from rke as root
-mkdir /root/.ssh
-chmod 700 /root/.ssh/
-chown root:root /root/.ssh
-cat /tmp/id_rsa.pub >> /root/.ssh/authorized_keys
-chmod 400 /root/.ssh/authorized_keys
-chown root:root /root/.ssh/authorized_keys
